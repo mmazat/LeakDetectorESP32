2e0b4530754ccbf67e5a9d47e037c0f24e03df49
mmazat <6835356+mmazat@users.noreply.github.com>
2020-10-05 23:09:05 -0400

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 
  *************************************************************/
 
-
 //#define USE_WROVER_BOARD
 #define USE_CUSTOM_BOARD // See "Custom board configuration" in Settings.h
 #define APP_DEBUG        // Comment this out to disable debug prints
@@ -20,12 +19,15 @@
 #include <stdint.h>
 #include <esp_deep_sleep.h>
 #include <driver/rtc_io.h>
+#include <esp_bt_main.h>
+#include <esp_bt.h>
+#include <esp_wifi.h>
 
 //sleep time between each measurement
 #define DEEP_SLEEP_TIME 3600 * 12 * 10e6 //microsec
 #define WATCH_DOG_TIMEOUT 12e6           //microsec
 #define CONFIG_PIN 32                    //pin to go to config mode
-const gpio_num_t wakeup_port=gpio_num_t::GPIO_NUM_33;
+const gpio_num_t wakeup_port = gpio_num_t::GPIO_NUM_33;
 
 #define MSG_LEAK "Leak Detected."
 #define MSG_SLEEP "Slept again."
@@ -38,6 +40,11 @@ const gpio_num_t wakeup_port=gpio_num_t::GPIO_NUM_33;
  * Source: Jack Rickard https://www.youtube.com/watch?v=7kLy2iwIvy8
  */
 hw_timer_t *watchdogTimer = NULL;
+void hibernate()
+{
+  esp_wifi_stop();
+  esp_deep_sleep_start();
+}
 bool configMode = false;
 void interruptReboot()
 {
@@ -48,12 +55,14 @@ void interruptReboot()
 
   Serial.println("Watchdog timed out!");
   delay(200);
-  esp_deep_sleep_start();
+ hibernate();
 }
+
 
 void setup()
 {
-  delay(500);
+  esp_bluedroid_disable();
+  esp_bt_controller_disable();
   Serial.begin(115200);
   BlynkProvisioning.begin();
 
@@ -64,9 +73,9 @@ void setup()
 
   //gpio_pullup_en(GPIO_NUM_34);
   rtc_gpio_init(wakeup_port);
-  rtc_gpio_set_direction(wakeup_port,rtc_gpio_mode_t::RTC_GPIO_MODE_INPUT_ONLY);
-  rtc_gpio_pullup_en(wakeup_port);
-  esp_sleep_enable_ext0_wakeup(wakeup_port, 0); //0 low 1 high
+  rtc_gpio_set_direction(wakeup_port, rtc_gpio_mode_t::RTC_GPIO_MODE_INPUT_ONLY);
+  //rtc_gpio_pullup_en(wakeup_port);
+  esp_sleep_enable_ext0_wakeup(wakeup_port, 1); //0 low 1 high
 
   //enable watch dog timer, to sleep mcu no matter what
   watchdogTimer = timerBegin(0, 80, true);                  //timer 0 divisor 80
@@ -76,6 +85,10 @@ void setup()
 
   //By default, esp_deep_sleep_start function will power down all RTC power domains which are not needed by the enabled wakeup sources
   // esp_deep_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+  esp_sleep_pd_config(ESP_PD_DOMAIN_MAX, ESP_PD_OPTION_OFF);  
   esp_sleep_enable_timer_wakeup(DEEP_SLEEP_TIME);
   delay(100);
 }
@@ -148,5 +161,5 @@ void loop()
     delay(200); // delay to make sure data is sent
   }
 
-  esp_deep_sleep_start();
+hibernate();
 }
